43bda5c3556b5cc0662fac78f9da02b0714c457f
Remy Guts <remyguts@gmail.com>
2020-02-19 19:55:49 -0800

    create server connection

--- employees.js
+++ employees.js
@@ -1,5 +1,14 @@
 "use strict";
-
+//create dependencies
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 const consoleTable = require("console.table");
+
+// create connection with server
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "otis1978",
+  database: "employees_db"
+});
