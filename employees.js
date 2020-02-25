a171ffd5b5a5a805b8ff44fb8e8367d76348d6c5
Remy Guts <remyguts@gmail.com>
2020-02-25 11:52:19 -0800

    inquirer functions

--- employees.js
+++ employees.js
@@ -19,4 +19,94 @@ connection.connect(function(err) {
   runSearch();
 });
 
-//
+//function case/break using inquirer
+function runSearch() {
+  inquirer
+    .prompt({
+      type: "list",
+      name: "action",
+      message: "What would you like to do?",
+      choices: [
+        "View all employees",
+        "View all departments",
+        "View all managers",
+        "Add Employee",
+        "Add Department",
+        "Add Role",
+        "Remove Employee",
+        "Update Employee Role",
+        "Update Employee Manager",
+        "Exit"
+      ]
+    })
+    .then(function(answer) {
+      console.log(answer.action);
+      switch (answer.action) {
+        case "View all employees":
+          employeeView();
+          break;
+
+        case "View all departments":
+          departmentView();
+          break;
+
+        case "View all managers":
+          managerView();
+          break;
+
+        case "Add Employee":
+          employeeAdd();
+          break;
+
+        case "Add Department":
+          departmentAdd();
+          break;
+
+        case "Add Role":
+          roleAdd();
+          break;
+
+        case "Remove Employee":
+          employeeRemove();
+          break;
+
+        case "Update Employee Role":
+          employeeUpdate();
+          break;
+
+        case "Exit":
+          connection.end();
+          break;
+      }
+    });
+}
+
+//create next inquiry for employee search by name
+function employeeView() {
+  inquirer
+    .prompt({
+      name: "employeeView",
+      type: "input",
+      message: "What employee would you like to search for (by last name)?"
+    })
+    .then(function(answer) {
+      var query = "SELECT first_name, last_name, id FROM employee WHERE ?";
+      connection.query(query, { last_name: answer.employeeView }, function(
+        err,
+        res
+      ) {
+        for (var i = 0; i < res.length; i++) {
+          console.log(
+            "First Name: " +
+              res[i].first_name +
+              " || Last name: " +
+              res[i].last_name +
+              " || Id: " +
+              res[i].id
+          );
+        }
+
+        runSearch();
+      });
+    });
+}
